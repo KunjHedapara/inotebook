c5ab35b494ad3c0103a79a3e36db488bf0ed9f82
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-03-07 10:52:34 +1200

    changes in backend

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&w=false"
+
+const connectToMongo = () => {
+    // mongoose.connect(mongoURI), () => {
+    //     console.log('Connect to mongo successfully');
+    // })
+    mongoose.connect(mongoURI)
+        .then(() =>
+            console.log("Connect to mongo successfully "))
+        .catch((e) => console.log(e.message))
+}
+
+module.exports = connectToMongo;
