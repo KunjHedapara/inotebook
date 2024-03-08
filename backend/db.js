86c7b83ff2a656f452fa7d30bb76cfbf22bfbb8d
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-03-08 16:26:16 +1200

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&w=false"
 
 const connectToMongo = () => {
