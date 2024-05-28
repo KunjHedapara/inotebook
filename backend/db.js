d3ebad90abaccff1d933cb72c4e7d4b12e666881
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-05-28 16:46:06 +1200

    changes

--- backend/db.js
+++ backend/db.js
@@ -5,21 +5,6 @@ const mongoURI = "mongodb://localhost:27017/inotebook";
 const connectToMongo = () => {
     mongoose.connect(mongoURI),
         console.log('Connected to mongo successfully');
-
-    //    mongoose.connect(mongoURI)
-    //        .then(() =>
-    //            console.log("Connect to mongo successfully "))
-    //       .catch((e) => console.log(e.message))
 }
 
 module.exports = connectToMongo;
-
-// const mongoose = require("mongoose");
-// const mongoURI = "mongodb://localhost:27017/";
-
-// const connectToMongo = () => {
-// mongoose.connect(mongoURI);
-// console.log("connected to mongo");
-// };
-
-// module.exports = connectToMongo;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-// const { isEmail } = require('validator');
-// const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
  
 const UserSchema = new Schema({
@@ -12,10 +10,6 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        // validate: {
-        //     validator: isEmail,
-        //     message: 'Please enter a valid email'
-        // }
     },
     password: {
         type: String,
@@ -26,16 +20,6 @@ const UserSchema = new Schema({
         default: Date.now
     },
 });
-
-// custom error heandling
-
-// UserSchema.post('save', function (error, doc, next) {
-//     if (error.name === 'MongoServerError' && error.code === 11000) {
-//         next(new Error('Email already exists'));
-//     } else {
-//         next(error);
-//     }
-// });
 
 const User = mongoose.model('user', UserSchema);
 User.createIndexes
