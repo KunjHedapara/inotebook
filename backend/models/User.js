5bf4bf1cabeba73a1a6afa27c5d26ed1fd027df8
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-05-28 16:42:00 +1200

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcryptjs');
+// const { isEmail } = require('validator');
+// const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
  
 const UserSchema = new Schema({
@@ -12,10 +12,10 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        validate: {
-            validator: isEmail,
-            message: 'Please enter a valid email'
-        }
+        // validate: {
+        //     validator: isEmail,
+        //     message: 'Please enter a valid email'
+        // }
     },
     password: {
         type: String,
@@ -29,14 +29,14 @@ const UserSchema = new Schema({
 
 // custom error heandling
 
-UserSchema.post('save', function (error, doc, next) {
-    if (error.name === 'MongoServerError' && error.code === 11000) {
-        next(new Error('Email already exists'));
-    } else {
-        next(error);
-    }
-});
+// UserSchema.post('save', function (error, doc, next) {
+//     if (error.name === 'MongoServerError' && error.code === 11000) {
+//         next(new Error('Email already exists'));
+//     } else {
+//         next(error);
+//     }
+// });
 
 const User = mongoose.model('user', UserSchema);
-
+User.createIndexes
 module.exports = User;
