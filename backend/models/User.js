d0bae04c09deedf12c8cd05f7ad06b23b0ed49d0
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-06-05 14:46:16 +1200

    commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
- 
+
 const UserSchema = new Schema({
-    name: {
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
         type: String,
         required: true,
+        unique: true
     },
-    email: {
+    password:{
         type: String,
-        required: true,
-        unique: true,
+        required: true
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    date: {
+    date:{
         type: Date,
         default: Date.now
     },
-});
-
-const User = mongoose.model('user', UserSchema);
-User.createIndexes
-module.exports = User;
+  });
+  const User = mongoose.model('user', UserSchema);
+  module.exports = User;
